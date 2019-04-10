851638ac8c8fe4c0022d2cbdfee46efb910e8a05
jonaselan <jonas.elan@gmail.com>
2019-04-10 19:01:19 -0300

    Create index action for categories

--- src/main/java/com/example/api/resources/CategoryResources.java
+++ src/main/java/com/example/api/resources/CategoryResources.java
@@ -1,8 +1,13 @@
 package com.example.api.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.api.domain.Category;
 
 // controller rest
 @RestController
@@ -12,7 +17,14 @@ public class CategoryResources {
 
 	// é preciso relacionar a função com o método http
 	@RequestMapping(method=RequestMethod.GET)
-	public String index() {
-		return "sim";
+	public List index() {
+		Category cat1 = new Category(1, "Sim");
+		Category cat2 = new Category(1, "Não");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
