0a2b794387f8890e89649ba6d864e6989774f6e8
jonaselan <jonas.elan@gmail.com>
2019-04-10 19:25:47 -0300

    Configure db (H2)

--- src/main/java/com/example/api/domain/Category.java
+++ src/main/java/com/example/api/domain/Category.java
@@ -2,9 +2,18 @@ package com.example.api.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// entidade do JPA
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
